7e7fef2de33446534877f38dee19ae913330e865
Komal <2888065k@student.gla.ac.uk>
2024-02-26 19:28:19 +0000

    Added page redirections

--- rateMyUogCourse/urls.py
+++ rateMyUogCourse/urls.py
@@ -6,4 +6,7 @@ app_name = 'rateMyUogCourse'
 
 urlpatterns = [
 path('login/', views.login, name='login'),
+path('signup/', views.signup, name='signup'),
+path('feedback/', views.feedback, name='feedback'),
+path('searchCourses/<str:course_name>/<str:program_type>', views.search, name = 'search')
 ]
--- rateMyUogCourse/views.py
+++ rateMyUogCourse/views.py
@@ -1,12 +1,41 @@
 from django.shortcuts import render
+from rateMyUogCourse.models import CourseSearchTable
+
+from lecturer.models import Course
 
 # Create your views here.
 from django.http import HttpResponse
 
 
 def mainPage(request):
- return HttpResponse("Main Page")
+ return render(request, 'rateMyUogCourse/homepage.html')
 
 
 def login(request):
- return HttpResponse("login")
+ return render(request, 'rateMyUogCourse/login.html')
+
+def signup(request):
+ return render(request, 'rateMyUogCourse/signup.html')
+
+def feedback(request):
+ return render(request, 'rateMyUogCourse/feedbackPage.html')
+
+
+def search(request, course_name, program_type):
+
+    context_dict={}
+    search_results = None
+
+    if(course_name == '' and program_type == ''):
+        search_results =  CourseSearchTable.objects.all()
+ 
+    try:
+        course_of_program_type = Course.objects.get(programType = program_type)
+        
+        search_results = course_of_program_type.courseSearchTable_set.filter(string__contains=course_name )
+    except:
+        pass
+
+    context_dict['search_results'] = search_results
+
+    return  render(request, 'rateMyUogCourse/course_rating_overview.html', context=context_dict)
