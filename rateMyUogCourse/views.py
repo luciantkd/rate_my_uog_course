581e926c818cddc3b2a15de7b92369c1f77dd781
Yu-Wei, Lu <lulucat861016@gmail.com>
2024-03-05 03:06:42 +0000

    signup and login view

--- rateMyUogCourse/views.py
+++ rateMyUogCourse/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from rateMyUogCourse.models import CourseSearchTable
 
 from lecturer.models import Course
 from rateMyUogCourse.forms import WebsiteFeedback
+from student.models import Student
+from lecturer.models import Lecturer
+from administrator.models import Admin
+import hashlib
 
 # Create your views here.
 from django.http import HttpResponse
@@ -10,13 +14,53 @@ from django.http import HttpResponse
 
 def mainPage(request):
  return render(request, 'rateMyUogCourse/homepage.html')
+    
+def checkPassword(inputPassword, userPassword):
+    return hashlib.sha256(inputPassword.encode('utf-8')).hexdigest() == userPassword
 
+def user_login(request):
 
-def login(request):
- return render(request, 'rateMyUogCourse/login.html')
+    if request.method == 'POST':
 
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = Student.objects.filter(email=email)[0]
+        # print(checkPassword(password, user.password))
+        
+        if user and checkPassword(password, user.password):
+            request.session['user_email'] = email
+            return redirect(mainPage)
+            
+        else:
+            return HttpResponse('Invalid login credentials')
+
+    return render(request, 'rateMyUogCourse/login.html')
+
+ 
 def signup(request):
- return render(request, 'rateMyUogCourse/signup.html')
+
+    if request.method == 'POST':
+
+        email = request.POST.get('email')
+        name = request.POST.get('name')
+        password = request.POST.get('password')
+        programType = request.POST.get('program_type')
+        guid = email.split("@")[0]
+
+        hashed_password = hashlib.sha256(password.encode('utf-8'))
+
+        Student.objects.create(
+            guid = guid,
+            email = email,
+            name = name,
+            password = hashed_password.hexdigest(),
+            programType = programType,
+        )
+
+        return redirect(reverse('rateMyUogCourse:login'))  
+
+    return render(request, 'rateMyUogCourse/signup.html') 
+ 
 
 def feedback(request):
  return render(request, 'rateMyUogCourse/feedbackPage.html')
