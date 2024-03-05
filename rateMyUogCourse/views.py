fa25fcf924ff4ed892fe755a95de34e630ce4420
Yu-Wei, Lu <lulucat861016@gmail.com>
2024-03-05 14:06:23 +0000

    refine signup and login logic

--- rateMyUogCourse/views.py
+++ rateMyUogCourse/views.py
@@ -16,6 +16,7 @@ def mainPage(request):
  return render(request, 'rateMyUogCourse/homepage.html')
     
 def checkPassword(inputPassword, userPassword):
+    # check if the input password equals the user's password
     return hashlib.sha256(inputPassword.encode('utf-8')).hexdigest() == userPassword
 
 def user_login(request):
@@ -24,20 +25,37 @@ def user_login(request):
 
         email = request.POST.get('email')
         password = request.POST.get('password')
-        user = Student.objects.filter(email=email)[0]
-        # print(checkPassword(password, user.password))
+        # user = Student.objects.filter(email = email)[0]
         
-        if user and checkPassword(password, user.password):
+        if Student.objects.filter(email = email):
+            user = Student.objects.filter(email = email)[0]
+            checkPassword(password, user.password)
             request.session['user_email'] = email
+            # TODO: redirect to student page
+            return redirect(mainPage)
+            
+        elif Lecturer.objects.filter(email = email):
+            user = Lecturer.objects.filter(email = email)[0]
+            checkPassword(password, user.password)
+            request.session['user_email'] = email
+            # TODO: redirect to lecturer page
+            return redirect(mainPage)
+            
+        elif Admin.objects.filter(email = email):
+            user = Admin.objects.filter(email = email)[0]
+            checkPassword(password, user.password)
+            request.session['user_email'] = email
+            # TODO: redirect to administrator page
             return redirect(mainPage)
             
         else:
-            return HttpResponse('Invalid login credentials')
+            return HttpResponse('Invalid login details')
 
     return render(request, 'rateMyUogCourse/login.html')
 
  
 def signup(request):
+    # only Student can sign up from website
 
     if request.method == 'POST':
 
@@ -45,19 +63,36 @@ def signup(request):
         name = request.POST.get('name')
         password = request.POST.get('password')
         programType = request.POST.get('program_type')
-        guid = email.split("@")[0]
+        confirmPassword = request.POST.get('confirm_password')
+        
+        # check if the email is already signed update
+        if Student.objects.filter(email=email):
+            return HttpResponse('Already signed up')
+        else:
+            # check if the email is UofG student
+            emailDomain = email.split("@")[1]
+            if emailDomain != 'student.gla.ac.uk':
+                return HttpResponse('You are not a student of UofG.')
+            
+            # check if the password is the same
+            if password == confirmPassword:
+            
+                guid = email.split("@")[0]
+                # hash the password
+                hashed_password = hashlib.sha256(password.encode('utf-8'))
 
-        hashed_password = hashlib.sha256(password.encode('utf-8'))
-
-        Student.objects.create(
-            guid = guid,
-            email = email,
-            name = name,
-            password = hashed_password.hexdigest(),
-            programType = programType,
-        )
-
-        return redirect(reverse('rateMyUogCourse:login'))  
+                Student.objects.create(
+                    guid = guid,
+                    email = email,
+                    name = name,
+                    password = hashed_password.hexdigest(),
+                    programType = programType,
+                )
+                return redirect(reverse('rateMyUogCourse:login'))
+                
+            else:
+                # TODO: if password not the same
+                return render(request, 'rateMyUogCourse/signup.html')
 
     return render(request, 'rateMyUogCourse/signup.html') 
  
