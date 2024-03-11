4e5094376420fd311ddea35a4fb978a666777a91
Elvis LIN <lzh000307@126.com>
2024-03-11 23:53:48 +0000

    Refactoring the lecturer page

--- administrator/view/course_view.py
+++ administrator/view/course_view.py
@@ -46,29 +46,6 @@ def course_management(request):
 
     return render(request, 'administrator/course_management.html', {'courses': courses})
 
-# def course_management(request):
-#     '''
-#     This method is to show the course management page
-#     :param request:
-#     :return: ourses a list of courses
-#     '''
-#     # courses = Course.objects.all().values('courseId', 'courseName', 'programType', 'semester', 'lecturercourseassignment__lecturerId__lecturerName').order_by('courseId')
-#     courses_list = Course.objects.all().order_by('courseId')
-#     courses = []
-#
-#     for course in courses_list:
-#         lecturer_names = course.lecturercourseassignment_set.all().values_list('lecturerId__lecturerName', flat=True)
-#         lecturer_names_str = ', '.join(lecturer_names)
-#
-#         courses.append({
-#             'courseId': course.courseId,
-#             'courseName': course.courseName,
-#             'programType': course.programType,
-#             'semester': course.semester,
-#             'lecturercourseassignment__lecturerId__lecturerName': lecturer_names_str
-#         })
-#     return render(request, 'administrator/course_management.html', {'courses': courses})
-
 
 # Admin - Course Edit Page
 def course_edit(request):
@@ -149,53 +126,6 @@ def course_edit(request):
 
 
 
-# Admin - Course Edit Post
-# This method is to update or create a course, using post method
-# @param request: the request
-# @param course: an entity of course
-# @return: boolean success
-def course_edit_post(request):
-#     if request.method == 'POST':
-#         course_id = request.POST.get('course_id', '').strip()
-#         course_name = request.POST.get('course_name', '').strip()
-#         program_type = request.POST.get('program_type', '').strip()
-#         lecturer_names = [name.strip() for name in request.POST.get('professor', '').split(',')]
-#         semester = request.POST.get('semester')
-#         if semester is None:
-#             semester = 2023
-#
-#         # 仅在所有验证通过后更新或创建课程
-#         course, created = Course.objects.update_or_create(
-#             courseId=course_id,
-#             defaults={'courseName': course_name, 'programType': program_type, 'semester': semester}
-#         )
-#
-#         # 处理讲师名字
-#         for lecturer_name in lecturer_names:
-#             try:
-#                 lecturer = Lecturer.objects.get(lecturerName=lecturer_name)
-#                 LecturerCourseAssignment.objects.update_or_create(lecturerId=lecturer, courseId=course)
-#             except Lecturer.DoesNotExist:
-#                 # 如果找不到讲师，向用户显示错误消息
-#                 messages.error(request, f'Lecturer "{lecturer_name}" not found. Please check the name and try again.')
-#                 # 返回到编辑页面，附带之前输入的信息，以便用户可以更正
-#                 return render(request, 'administrator/course_edit.html', {
-#                     'course': {
-#                         'courseId': course_id,
-#                         'courseName': course_name,
-#                         'programType': program_type,
-#                         'semester': semester,
-#                         'lecturerName': ', '.join(lecturer_names)
-#                     }
-#                 })
-#
-#         return redirect(reverse('administrator:course_management'))
-#
-#     # 如果请求方法不是POST，重定向到某个视图
-    return redirect(reverse('administrator:course_management'))
-
-
-
 # Admin - Course Delete
 # This method is to delete a course
 # @param course_id: the id of the course
@@ -216,23 +146,3 @@ def course_delete(request):
 #     return render(request, 'administrator/course_edit.html')
 def course_add(request):
     return render(request, 'administrator/course_edit.html')
-
-def course_add_post(request):
-#     if request.method == "POST":
-#         course_id = request.POST.get('course_id')
-#         course_name = request.POST.get('course_name')
-#         program_type = request.POST.get('program_type')
-#         semester = request.POST.get('semester')
-#         lecturer_name = request.POST.get('lecturer_name')
-#         if semester is None:
-#             semester = 2023
-#         course = Course(courseId=course_id, courseName=course_name, programType=program_type, semester=semester)
-#         course.save()
-#         if lecturer_name is not None:
-#             lecturer = Lecturer.get_object_or_404(Lecturer, lecturerName=lecturer_name)
-#             lecturer_course_assignment = LecturerCourseAssignment(lecturerId=lecturer.lecturerId, courseId=course)
-#             lecturer_course_assignment.save()
-#         # create search table for the course
-#         course_search = CourseSearchTable(courseId=course, courseName=course_name)
-#         course_search.save()
-        return redirect(reverse('administrator:course_management'))
