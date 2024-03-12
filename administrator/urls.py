5bcaa723c267749028868f8fdc332a6840c451cc
Elvis LIN <lzh000307@126.com>
2024-03-12 01:17:35 +0000

    fix report review management

--- administrator/urls.py
+++ administrator/urls.py
@@ -10,7 +10,8 @@ urlpatterns = [
     path ('base/', views.base, name='base'),
     path ('report_review_management/', views.reported_reviews_management, name='report_review_management'),
     path ('report_review_detail/', views.reported_review_detail, name='report_review_detail'),
-    path ('report_review_delete/', views.reported_review_delete, name='report_review_delete'),
+    path('reported_review_delete/<int:feedback_id>/', views.reported_review_delete, name='reported_review_delete'),
+    path('reported_review_approve/<int:feedback_id>/', views.reported_review_approve, name='reported_review_approve'),
     # website feedback part
     path ('website_feedback_management/', feedback_view.website_feedback_management, name='website_feedback_management'),
     path ('website_feedback_detail/', feedback_view.website_feedback_detail, name='website_feedback_detail'),
--- administrator/views.py
+++ administrator/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
@@ -16,10 +18,20 @@ def mainPage(request):
 
 def reported_reviews_management(request):
     # get course feedbacks where reported != 0, and join with course names, and sort by reported
-    course_feedbacks = CourseFeedback.objects.all().values('feedbackId', 'courseId', 'courseId__courseName', 'courseId__coursesearchtable__reviews',
-                                                           'reported').filter(reported__gt=0).order_by('reported')
-    print(course_feedbacks)
-    return render(request, "administrator/reported_reviews_management.html", {'course_feedbacks': course_feedbacks})
+    course_feedbacks = CourseFeedback.objects.all().values('feedbackId', 'courseId', 'reported').filter(reported__gt=0)
+    # search for course name, program type
+    course_feedbacks_view = []
+    for feedback in course_feedbacks:
+        course = Course.objects.get(courseId=feedback['courseId'])
+        course_feedbacks_view.append({
+            'feedbackId': feedback['feedbackId'],
+            'courseId': feedback['courseId'],
+            'courseName': course.courseName,
+            'programType': course.programType,
+            'reported': feedback['reported'],
+        })
+    # print(course_feedbacks_view)
+    return render(request, "administrator/reported_reviews_management.html", {'course_feedbacks': course_feedbacks_view})
     # return render(request, reverse('administrator:reported_reviews_management'), {'course_feedbacks': course_feedbacks})
 
     # return render(request, 'report_review_management.html')
@@ -31,10 +43,13 @@ def reported_reviews_management(request):
 def reported_review_detail(request):
     feedback_id = request.GET.get('feedback_id')
     feedback_entity = get_object_or_404(CourseFeedback, pk=feedback_id)
-    # get the course name                                                                                                   
-    course_name = Course.objects.get(courseId=feedback_entity.courseId).courseName
-    return render(request, 'report_review_detail.html',
-                  {'feedback_entity': feedback_entity, 'course_name': course_name})
+    # get the course name
+    print(feedback_entity.courseId_id)
+    course = Course.objects.get(courseId=feedback_entity.courseId_id)
+    course_name = course.courseName
+    # semester = course.semester
+    return render(request, 'administrator/admin_report_review_detail.html',
+                  {'feedback_entity': feedback_entity, 'course_name': course_name, })
 
 # Admin - Delete Reported Review
 # This method is to delete a reported review
@@ -45,9 +60,17 @@ def reported_review_delete(request):
     feedback_entity = get_object_or_404(CourseFeedback, pk=feedback_id)
     if feedback_entity is not None:
         feedback_entity.delete()
-        return render(request, 'report_review_detail.html', {'success': True})
-    else:
-        return render(request, 'report_review_detail.html', {'success': False})
+    return redirect(reverse('administrator:report_review_management'))
+
+def reported_review_approve(request, feedback_id):
+    print(feedback_id)
+    feedback = get_object_or_404(CourseFeedback, feedbackId=feedback_id)
+    feedback.reported = 0
+    # print(feedback.feedbackDateTime)
+    # feedback.feedbackDateTime = datetime.now()
+    feedback.save()
+    return redirect(reverse('administrator:report_review_management'))
+
 
 
 
