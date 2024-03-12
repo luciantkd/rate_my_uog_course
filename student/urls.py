1181e2808f3e9b4e2929024d939da6fdc3df29dc
@2867781P <2867781p@student.gla.ac.uk>
2024-03-12 09:07:28 +0000

    fixed student urls.py

--- student/urls.py
+++ student/urls.py
@@ -8,7 +8,6 @@ app_name = 'student'
 urlpatterns = [
     path('detail/<str:course_Id>/<str:guId>', studentViews.show_detailed_rating, name='show_detailed_rating'),
     path('searchCourses', views.search, name = 'search'),
-    path('save_feedback/<str:course_id>/<str:guId>',studentViews.save_feedback, name = 'save_feedback')
-    
+    path('save_feedback/<str:course_id>/<str:guId>',studentViews.save_feedback, name = 'save_feedback'),
     path('report_feedback/<int:feedback_id>/', studentViews.report_feedback, name='report_feedback'),
 ]
