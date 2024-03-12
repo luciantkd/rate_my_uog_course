96baf90343b16febd6f0e084f0ea4f2b4d23f7ab
@2867781P <2867781p@student.gla.ac.uk>
2024-03-12 07:43:58 +0000

    Added report a review functionality

--- student/urls.py
+++ student/urls.py
@@ -8,5 +8,5 @@ app_name = 'student'
 urlpatterns = [
     path('detail/<str:course_Id>/<str:guId>', studentViews.show_detailed_rating, name='show_detailed_rating'),
     path('searchCourses', views.search, name = 'search'),
-    
+    path('report_feedback/<int:feedback_id>/', studentViews.report_feedback, name='report_feedback'),
 ]
