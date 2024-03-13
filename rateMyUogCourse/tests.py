04ece706839e8007375ea6b6e80fba40ec52def3
Yu-Wei, Lu <lulucat861016@gmail.com>
2024-03-13 23:46:11 +0000

    add rateMyUogCourse, student, lecturer testcases

--- rateMyUogCourse/tests.py
+++ rateMyUogCourse/tests.py
@@ -1,1 +1,80 @@
+from django.test import TestCase, Client, RequestFactory
+from django.urls import reverse
+from django.utils import timezone
+from rateMyUogCourse.views import user_login, encryptPassword
+from lecturer.models import Course, Lecturer, LecturerCourseAssignment
+from student.models import Student, CourseFeedback
+from administrator.models import Admin
+from unittest.mock import MagicMock, patch
+
 # Create your tests here.
+class UserLoginTestCase(TestCase):
+    def setUp(self):
+        
+        self.factory = RequestFactory()
+        self.student_user_data = {
+            'g-recaptcha-response': 'valid_token',
+            'email': 'student@test.com',
+            'password': 'testpassword'
+        }
+        self.lecturer_user_data = {
+            'g-recaptcha-response': 'valid_token',
+            'email': 'lecturer@test.com',
+            'password': 'testpassword'
+        }
+        self.admin_user_data = {
+            'g-recaptcha-response': 'valid_token',
+            'email': 'admin@test.com',
+            'password': 'testpassword'
+        }
+        # Create a user for each role
+        self.student = Student.objects.create(email=self.student_user_data['email'], password=encryptPassword(self.student_user_data['password']))
+        self.lecturer = Lecturer.objects.create(lecturerId = self.lecturer_user_data['email'].split('@')[0],email=self.lecturer_user_data['email'], password=encryptPassword(self.lecturer_user_data['password']))
+        self.admin = Admin.objects.create(email=self.admin_user_data['email'], password=encryptPassword(self.admin_user_data['password']))
+
+    def test_login_page_render(self):
+        # Test rendering of login page
+
+        request = self.factory.post(reverse('rateMyUogCourse:login'))
+        response = user_login(request)
+        self.assertEqual(response.status_code, 200)
+        # self.assertTemplateUsed(response, 'rateMyUogCourse/login.html')
+        
+    @patch('rateMyUogCourse.views.requests.post')
+    def test_user_login_student(self, mock_post):
+        # Test login as student
+        mock_response = MagicMock()
+        mock_response.json.return_value = {'success': True}
+        mock_post.return_value = mock_response
+        
+        request = self.factory.post(reverse('rateMyUogCourse:login'), data=self.student_user_data)
+        request.session = {}
+        response = user_login(request)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(request.session['user_type'], 'student')
+        
+    @patch('rateMyUogCourse.views.requests.post')
+    def test_user_login_lecturer(self, mock_post):
+        # Test login as lecturer
+        mock_response = MagicMock()
+        mock_response.json.return_value = {'success': True}
+        mock_post.return_value = mock_response
+        
+        request = self.factory.post(reverse('rateMyUogCourse:login'), data=self.lecturer_user_data)
+        request.session = {}
+        response = user_login(request)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(request.session['user_type'], 'lecturer')
+        
+    @patch('rateMyUogCourse.views.requests.post')
+    def test_user_login_admin(self, mock_post):
+        # Test login as admin
+        mock_response = MagicMock()
+        mock_response.json.return_value = {'success': True}
+        mock_post.return_value = mock_response
+        
+        request = self.factory.post(reverse('rateMyUogCourse:login'), data=self.admin_user_data)
+        request.session = {}
+        response = user_login(request)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(request.session['user_type'], 'administrator')
--- student/tests.py
+++ student/tests.py
@@ -1,1 +1,66 @@
-# Create your tests here.
+from django.test import TestCase, RequestFactory
+from django.urls import reverse
+from django.http import JsonResponse
+import json
+
+from student.views import save_feedback, show_detailed_rating, report_feedback, like_feedback
+from student.models import Course, CourseFeedback, Student, StudentFeedbackLikes
+from rateMyUogCourse.models import CourseSearchTable
+
+class ShowDetailedRatingTestCase(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.student = Student.objects.create(guid='testid')
+        self.course = Course.objects.create(courseId=1, courseName='123', semester = 1)
+        self.course_feedback = CourseFeedback.objects.create(courseId=self.course, overall=4, difficulty=3,
+                                                                     usefulness=4, workload=3,
+                                                                     recommendCourse=True, lecturerRating=4, guid = self.student)
+        self.courseSearchTable = CourseSearchTable.objects.create(courseId = self.course, courseName = self.course.courseName, 
+        overall = 1, difficulty = 1, usefulness = 1, workload = 1, reviews = 1, wouldRecommend = True, professorRating = 1)
+
+
+    def test_show_detailed_rating(self):
+        url = reverse('student:show_detailed_rating', kwargs={'course_Id': self.course.courseId,
+                                                      'guId': 'testid'})
+        request = self.factory.get(url)
+        request.session = {'user_type': 'student', 'user_id':self.student.guid}
+        response = show_detailed_rating(request, self.course.courseId, 'testid')
+        self.assertEqual(response.status_code, 200)
+
+
+class ReportFeedbackTestCase(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.student = Student.objects.create(guid='testid')
+        self.course = Course.objects.create(courseId=1, courseName='123', semester = 1)
+        self.feedback = CourseFeedback.objects.create(courseId=self.course, overall=4, difficulty=3,
+                                                             usefulness=4, workload=3,
+                                                             recommendCourse=True, lecturerRating=4, guid = self.student)
+
+    def test_report_feedback(self):
+        url = reverse('student:report_feedback', kwargs={'feedback_id': self.feedback.feedbackId})
+        request = self.factory.post(url)
+        response = report_feedback(request, self.feedback.feedbackId)
+        response_data = json.loads(response.content)
+        self.assertEqual(response_data, {'reported': True, 'reported_count': 1})
+
+
+
+class LikeFeedbackTestCase(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.course = Course.objects.create(courseId=1, courseName='123', semester = 1)
+        
+        self.student = Student.objects.create(guid='testid')
+        self.feedback = CourseFeedback.objects.create(courseId=self.course, overall=4, difficulty=3,
+                                                             usefulness=4, workload=3,
+                                                             recommendCourse=True, lecturerRating=4, guid = self.student)
+
+    def test_like_feedback(self):
+        url = reverse('student:like_feedback', kwargs={'feedback_id': self.feedback.feedbackId, 'guId': self.student.guid})
+        request = self.factory.post(url)
+        response = like_feedback(request, self.feedback.feedbackId, self.student.guid)
+        self.assertEqual(response.status_code, 200)
+        response_data = json.loads(response.content)
+        self.assertEqual(response_data, {'success': True})
+
